6fccda1f00eab412afd45788bbd1dc4b87996d22
Twisside <136237170+Twisside@users.noreply.github.com>
2024-11-10 23:24:10 +0200

    Delete TMPS lab_0 directory

--- TMPS lab_0/ITravel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TMPS_lab_1;
-
-public interface ITravel
-{
-    public void AvailableVehicles();
-    public void Board(string? vechicle);
-    public void Unboard(string? vechicle);
-    public void CalculateTax(string vechicle, int distance);
-}
